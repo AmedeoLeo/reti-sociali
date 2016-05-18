fa569219d4321d193604b65c8fb155134daf71aa
Francesco Gaetano <f.gaetano90@gmail.com>
2016-05-18 14:08:58 +0200

    modified impl_Parser

--- best_match/ass_2/Test_Parser.py
+++ best_match/ass_2/Test_Parser.py
@@ -5,32 +5,34 @@ from HTMLParser import HTMLParser
 #from re import findall, sub
 import re
 import sys
-import os
+
 reload(sys)
 
 tag = True
 parser = HTMLParser()
 sys.setdefaultencoding('UTF8')
 output = open("output.txt", "w")
-topic = "sport"
+topic = []
+topic.append("sport")
+topic.append("games")
+index=0
 
-for filename in os.listdir(os.getcwd()):
-    if filename.endswith(".pages"):
-        #topic = "test"
-        graph,db = read_wibbi("./"+filename, topic)
-        #graph,db = read_wibbi("./"+filename)
-        for topic in graph.keys():
-            toPrint = topic
-            for i in graph[topic].keys():
-                toPrint += i+ "["
-                for val in db[topic][i]:
-                    if '<' in val:
-                        tag = False
-                    elif '/>' in val:
-                        tag = True
-                    elif re.match("^[A-Za-z0-9]*$", val):
-                        html_decoded_string = parser.unescape(val)
-                        toPrint += html_decoded_string + ","
-                print >> output,  toPrint + "]"    
-            print >> output, "--------------------------------------------------------------------------------"
+toPrint = topic[index]+"\n"
+#index+=1
+#topic = "test"
+graph,db = read_wibbi()
+#graph,db = read_wibbi("./"+filename)
+for topic in graph.keys():
+    for i in graph[topic].keys():
+        toPrint += i+ "["
+        for val in db[topic][i]:
+            if '<' in val:
+                tag = False
+            elif '/>' in val:
+                tag = True
+            elif re.match("^[A-Za-z0-9]*$", val):
+                html_decoded_string = parser.unescape(val)
+                toPrint += html_decoded_string + ","
+    #print >> output,  toPrint + "]"    
+    #print >> output, "--------------------------------------------------------------------------------"
 output.close()
